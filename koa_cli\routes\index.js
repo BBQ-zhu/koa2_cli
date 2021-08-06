591a82f805b6c485f0985479cf0b29e252a9d2f4
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-06 19:01:59 +0800

    Create koa_cli\routes\index.js

---
+++ koa_cli\routes\index.js
@@ -0,0 +1,22 @@
+/**
+ * 公共接口文件
+ */
+const router = require('koa-router')()
+const config = require('../config')
+const jsonwebtoken = require('jsonwebtoken'); //加密生成token
+const {return200,return500} = require('../config/error')
+router.prefix(config.api)
+
+router.get('/', async (ctx, next) => {
+   ctx.body = '欢迎使用koa2!!!' // ctx.query
+})
+
+router.post('/checkToken', async (ctx) => {
+   // 中间件统一验证token
+   let token = ctx.header.authorization;
+   let payload = jsonwebtoken.verify(token.split(' ')[1], config.SECRET)
+   payload ? return200('token已认证',null,ctx) : return500('token已失效',null,ctx)
+})
+
+
+module.exports = router
