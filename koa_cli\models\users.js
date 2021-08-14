6eb2f8a6d011edf5cb4c062d8fd0a630125afac2
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-14 21:13:42 +0800

    Update koa_cli\models\users.js

--- koa_cli\models\users.js
+++ koa_cli\models\users.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     uid:String,
     username:String,
-    password:String,
-    phone:{type:String,default: ''}
+    password:{type:String,default: '123456'},
+    role:{type:String,default: ''},
+    phone:{type:String,default: ''},
+    imgurl:{type:String,default: ''},
 })
 
 const User = mongoose.model('users',userSchema)
