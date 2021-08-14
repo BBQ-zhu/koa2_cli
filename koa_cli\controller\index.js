e22df127a5fc57960cd3ddce05a28d98df955864
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-14 21:08:07 +0800

    Update koa_cli\controller\index.js

--- koa_cli\controller\index.js
+++ koa_cli\controller\index.js
@@ -7,56 +7,46 @@ const {
 } = require('../config/error')
 const jsonwebtoken = require('jsonwebtoken'); //加密生成token
 const config = require('../config')
+const {navMenu} = require('../models/navMenus')
+const fs = require('fs')
+const path = require('path')
 
-const create = (model, data, ctx) => (
-  model.create(data)
-  .then(rel => {
-    rel ? return200('新增成功', null, ctx) : return500('新增失败', null, ctx)
-  })
-  .catch(err => {
-    return500('新增失败', err, ctx)
-  })
-)
 
-const del = (model, data, ctx) => (
-  model.findOneAndDelete(data)
-  .then(rel => {
-    rel ? return200('删除成功', null, ctx) : return500('删除失败', null, ctx)
+//上传文件公共方法
+const uploadFile = (ctx, dir, name, path) => {
+  return new Promise(function (resolve, reject) {
+    // 创建文件输入流
+    const fileReader = fs.createReadStream(path);
+    // 文件将要的存放文件夹路径
+    const fileDir = `${__dirname}/../public/uploads/${dir}`;
+    // 判断目录是否存在,目录不存在则创建
+    if (!fs.existsSync(fileDir)) {
+      try {
+        fs.mkdirSync(fileDir);
+      } catch (e) {
+        reject(e)
+      }
+    }
+    // 保存文件的最终路径 (文件夹路径+文件名)
+    // localhost:3000/uploads/xxx.png
+    var FileName = `${name.split('.')[0]}-${Date.now()}.${name.split('.')[1]}` //时间戳
+    var FileName_dir = `${ctx.origin}/uploads/${dir}/${FileName}`; //ctx.origin上下文地址：http://localhost:3030
+    const filepath = `${fileDir}/${FileName}`;
+    // 创建文件输出流
+    const fileWriter = fs.createWriteStream(filepath);
+    // 写入文件数据
+    fileReader.pipe(fileWriter);
+    resolve(FileName_dir)
   })
-  .catch(err => {
-    return500('删除失败', err, ctx)
-  })
-)
+}
 
-const update = (model, id, data, ctx) => (
-  model.updateOne(id, data)
-  .then(rel => {
-    console.log(rel)
-    rel ? return200('更新成功', null, ctx) : return500('更新失败', null, ctx)
-  })
-  .catch(err => {
-    return500('更新失败', err, ctx)
-  })
-)
-
-const find = (model, data, ctx) => (
-  model.find(data)
-  .then(rel => {
-    rel ? return200('查询成功', rel, ctx) : return500('查询失败', null, ctx)
-  })
-  .catch(err => {
-    return500('查询失败', err, ctx)
-  })
-)
 
 const login = (model, data, ctx) => (
   model.findOne(data, {
     password: 0,
-    _id: 0,
     __v: 0
   })
   .then(rel => {
-    console.log(rel)
     if (rel) {
       let token = jsonwebtoken.sign({
           name: rel.username,
@@ -78,12 +68,100 @@ const login = (model, data, ctx) => (
   })
 )
 
+// 菜单导航
+let navMenuList = [
+  {
+    path: '/Home',
+    name:'网站管理',
+    id:1,
+    icon:'el-icon-location',
+    show: false,
+    component: '',
+    redirect: '',
+    children:[
+      {
+        path: '/WebControl',
+        name:'网站管理',
+        id:2,
+        icon:'el-icon-location',
+        show: false,
+        component: ''
+      },
+      {
+        path: '/VipUser',
+        name:'会员用户',
+        id:3,
+        icon:'el-icon-location',
+        show: false,
+        component: ''
+      },
+      {
+        path: '/EntrSchool',
+        name:'创业者学堂',
+        id:7,
+        icon:'el-icon-location',
+        show: false,
+        component: ''
+      },
+    ]
+  },
+  {
+    path: '/Home',
+    name:'权限管控',
+    id:4,
+    icon:'el-icon-location',
+    show: false,
+    component: 'Home',
+    redirect: '',
+    children:[
+      {
+        path: '/Works',
+        name:'员工管理',
+        id:5,
+        icon:'el-icon-location',
+        show: false,
+        component: 'Works'
+      },
+      {
+        path: '/Roles',
+        name:'角色管理',
+        id:6,
+        icon:'el-icon-location',
+        show: false,
+        component: 'Roles'
+      }
+    ]
+  }
+]
 
+//校验导航菜单是否已存在数据库，然后再登录
+const AddNavMenu = async (model, data,ctx)=>{
+  var newList = {
+    "navMenuList":navMenuList
+  }
+  await navMenu.findOneAndReplace({"name":"navMenu"},newList).then(async rel=>{
+    if(rel){
+      await login(model, data, ctx)
+    }else{
+      await navMenu.create({
+        "name": "navMenu",
+        "navMenuList": navMenuList
+      }).then(async rel=>{
+        if(rel){
+          await login(model, data, ctx)
+        }else{return500("创建导航菜单失败",null,ctx)}
+      }).catch(err=>{
+        return500("创建导航菜单失败",err,ctx)
+      })
+    }
+  }).catch(err=>{
+    console.log(err)
+    return500("创建导航菜单失败",err,ctx)
+  })
+}
 
 module.exports = {
-  create,
-  del,
-  update,
-  find,
-  login
+  AddNavMenu,
+  login,
+  uploadFile
 }
