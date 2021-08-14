516bc5d4a536b2ce970ee52b72bdc4e585e7f0f3
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-14 21:18:56 +0800

    Update koa_cli\app.js

--- koa_cli\app.js
+++ koa_cli\app.js
@@ -15,10 +15,18 @@ const MongoConnect = require('./db')() //建立数据库连接
 // 中间件对token进行验证 注意：放在路由前面
 app.use(koajwt({ secret: config.SECRET }).unless({
   // 登录接口不需要验证
-  path: [/^\/api\/user\/loginUser/,/^\/api\/user\/createUser/]   // 不需要做校验的接口在此处匹配  \/ 转译斜杠
+  path: [ // 不需要做校验的接口在此处匹配  \/ 转译斜杠
+    /^\/api\/user\/loginUser/,
+    /^\/api\/user\/createUser/,
+    /^\/api\/roles\/findNavMenus/,
+    /^\/api\/roles\/findRole/,
+    /^\/api\/roles\/findOneRole/,
+    /^\/uploads\/userImgs/,
+    /^\/uploads\/schoolVideo/
+  ]   
 }));
 
-//设置session
+//设置session 图形验证码
 app.keys = ['some secret hurr'];
 const SessionConfig={
     key:'koa:sess',
@@ -35,18 +43,36 @@ app.use(session(SessionConfig,app))
 //路由引入
 const index = require('./routes/index')
 const users = require('./routes/users')
+const roles = require('./routes/roles')
+const uploads = require('./routes/uploads')
+const informations = require('./routes/informations')
 
-
+app.use(KoaBody({ //放到路由下面，注意顺序
+  multipart: true,//是否允许上传文件
+  stict:false, //解析所有请求
+  formidable: {
+      // uploadDir:path.join(__dirname,'public/upload/'), // 设置文件上传目录,这里先不写，后面调用时动态创建目录
+      keepExtensions: true, // 保持文件的后缀
+      strict: false, 
+      multipart:true,
+      maxFileSize: 1024 * 1024 * 10 * 1024 // 设置上传文件大小最大限制，默认1G
+  },
+  onError:()=>{
+    ctx.body={
+      code:400,
+      data:'文件上传失败'
+    }
+  }
+}));
 // error 错误处理
 app.use(Cors());
 app.use(jsonerror())
-app.use(KoaBody());
 app.use(parameter(app))
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
-// logger打印日志
+// logger打印接口请求日志
 app.use(async (ctx, next) => {
   const start = new Date()
   await next()
@@ -57,6 +83,10 @@ app.use(async (ctx, next) => {
 // routes 路由注册
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+app.use(roles.routes(), users.allowedMethods())
+app.use(uploads.routes(), uploads.allowedMethods())
+app.use(informations.routes(), informations.allowedMethods())
+
 
 // error-handling
 app.on('error', (err, ctx) => {
