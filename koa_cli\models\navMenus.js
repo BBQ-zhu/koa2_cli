adcd48954eebb3716d58eb1f367961d98b719806
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-21 17:30:54 +0800

    Update koa_cli\models\navMenus.js

--- koa_cli\models\navMenus.js
+++ koa_cli\models\navMenus.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose')
 
+//菜单导航表
+const navMenuSchema = new mongoose.Schema({
+    name:{type:String,default:'navMenu'},
+    navMenuList:{type:Array,default:[]}
+})
+const navMenu = mongoose.model('navMenus',navMenuSchema)
+
+module.exports = {
+  navMenu
+}
