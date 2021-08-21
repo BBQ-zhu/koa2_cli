bc7a784a387c2bff7adae9d6683cbf9ff097fe82
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-21 17:31:44 +0800

    Update koa_cli\models\roles.js

--- koa_cli\models\roles.js
+++ koa_cli\models\roles.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose')
 
+//角色表
+const rolesSchema = new mongoose.Schema({
+  rolename:{type:String,default:''},
+  roleList:{type:Array,default:[]}
+})
+
+const roles = mongoose.model('roles',rolesSchema)
+
+module.exports = {
+  roles
+}
