77122f380d903e06f6467fde1be8f3be75acc437
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-21 17:34:45 +0800

    Update koa_cli\routes\informations.js

--- koa_cli\routes\informations.js
+++ koa_cli\routes\informations.js
@@ -1,1 +1,17 @@
+const router = require('koa-router')()
+const config = require('../config')
+const informations = require('../controller/informations')
+router.prefix(config.api + '/informations')
 
+//查询信息列表
+router.post('/findNews', informations.findNews) 
+//新建新闻信息
+router.post('/createNews', informations.createNews) 
+//更新新闻信息
+router.post('/updateNews', informations.updateNews) 
+//删除新闻信息
+router.post('/deleteNews', informations.deleteNews) 
+
+
+
+module.exports = router
