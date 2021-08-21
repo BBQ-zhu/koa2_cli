33c2f23d3d96f44b187ac1c9e23afed9697b524a
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-21 17:29:37 +0800

    Update koa_cli\models\informations.js

--- koa_cli\models\informations.js
+++ koa_cli\models\informations.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose')
 
+
+//新闻管理
+const informationSchema = new mongoose.Schema({
+  type:{type:String,default:'news'},
+  creators:{type:String,default:''},
+  newsname: String,
+  content:String,
+  time:{type:String,default:''},
+})
+const informations = mongoose.model('informations', informationSchema)
+
+module.exports = {
+  informations
+}
