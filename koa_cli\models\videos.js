28ec0bc6564fbd71f52b8d9279ecff74703c886a
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-21 17:32:58 +0800

    Update koa_cli\models\videos.js

--- koa_cli\models\videos.js
+++ koa_cli\models\videos.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose')
 
+const videoSchema = new mongoose.Schema({
+    fromid:{type:String,default: 'school'},
+    videoname:{type:String,default: 'school'},
+    videourl:{type:String,default: ''},
+    time:{type:String,default: ''}
+})
+
+const Video = mongoose.model('videos',videoSchema)
+module.exports = {
+  Video
+}
