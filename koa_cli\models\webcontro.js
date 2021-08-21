c67051cbff71024a7855561fda1908552e345810
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-21 17:33:14 +0800

    Update koa_cli\models\webcontro.js

--- koa_cli\models\webcontro.js
+++ koa_cli\models\webcontro.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose')
 
+const webSchema = new mongoose.Schema({
+    fromid:{type:String,default: 'school'},
+    videoname:{type:String,default: 'school'},
+    videourl:{type:String,default: ''},
+})
+
+const WebControl = mongoose.model('webcontrols',webSchema)
+module.exports = {
+  WebControl
+}
