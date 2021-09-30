75d1f9a61c2961fe3e3d56706a5a1a19b8b40719
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-09-30 19:00:06 +0800

    Update models\scroImg.js

--- models\scroImg.js
+++ models\scroImg.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 //轮播图表
 const scroImgSchema = new mongoose.Schema({
   typeid:{type:String,default:''},
+  link:String,
+  bgcolor:String,
   scroimg:String
 })
 
