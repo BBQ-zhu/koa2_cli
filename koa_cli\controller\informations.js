c3def503aef3b1d2fa36894a9acdacdbd56eea5b
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-09-30 18:47:41 +0800

    Update koa_cli\controller\informations.js

--- koa_cli\controller\informations.js
+++ koa_cli\controller\informations.js
@@ -12,6 +12,7 @@ const findNews = async (ctx) => {
   }
   await informations.aggregate([
     { $match : match }, //用于过滤数据
+    { $sort:{"time":-1} }, //倒叙排序
     { $project : {
         __v : 0
       }
@@ -32,7 +33,6 @@ const findNews = async (ctx) => {
     }
   ])
   .then(rel => {
-    console.log(rel)
     rel ? return200('新闻列表查询成功', rel, ctx) : return500('新闻列表查询失败', null, ctx)
   })
   .catch(err => {
@@ -41,7 +41,7 @@ const findNews = async (ctx) => {
 }
 
 const createNews = async (ctx) => {
-  let data = {newsname,content,creators} = ctx.request.body
+  let data = {newsname,message,num,creators,content} = ctx.request.body
   data.time = new Date().toLocaleString()
   await informations.create(data).then(rel => {
     return200('新闻添加成功',rel,ctx)
@@ -49,9 +49,8 @@ const createNews = async (ctx) => {
 }
 
 const updateNews = async (ctx) => {
-  let data = {newsname,content} = ctx.request.body
+  let data = {newsname,message,num,creators,content} = ctx.request.body
   let {_id} = ctx.request.body
-  console.log(data)
   await informations.findOneAndUpdate({_id},data).then(rel => {
     return200('新闻更新成功',rel,ctx)
   })
