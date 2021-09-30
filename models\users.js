a3a3007de59d8e43b2dfcab61b461061ac079600
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-09-30 19:00:56 +0800

    Update and rename koa_cli\models\users.js to models\users.js

---
+++ models\users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    uid:String, //员工ID
+    username:String, //名称
+    password:{type:String,default: '123456'},
+    role:{type:String,default: ''}, //角色
+    team:{type:String,default: ''}, //团队
+    phone:{type:String,default: ''}, //电话
+    imgurl:{type:String,default: ''}, //角色
+    idcard:String, //身份证
+    address:String, //家庭住址
+    isrecomed:String, //是否推荐至咨询顾问
+    intro:String, //顾问介绍
+    remarks:String, //备注
+    time:{type:String,default: ''},
+})
+
+const User = mongoose.model('users',userSchema)
+module.exports = {
+  User
+}
