d845bef0c96ca74def289aca433b030a0c1fa56d
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-09-30 18:56:18 +0800

    Update and rename koa_cli\models\informations.js to models\informations.js

--- models\informations.js
+++ models\informations.js
@@ -4,10 +4,12 @@ const mongoose = require('mongoose')
 //新闻管理
 const informationSchema = new mongoose.Schema({
   type:{type:String,default:'news'},
+  message:String,
+  num:String,
   creators:{type:String,default:''},
   newsname: String,
   content:String,
-  time:{type:String,default:''},
+  time:{type:String,default:''}
 })
 const informations = mongoose.model('informations', informationSchema)
 
