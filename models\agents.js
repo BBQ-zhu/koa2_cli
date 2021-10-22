2b0ffcb9759716fa50ac92ec56a21e0ea9eacfd2
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-10-22 15:50:21 +0800

    Update models\agents.js

--- models\agents.js
+++ models\agents.js
@@ -2,18 +2,24 @@ const mongoose = require('mongoose')
 
 //代办表
 const agentSchema = new mongoose.Schema({
-    proid:String, //产品id
-    type: {type: String,default: '其它'}, //数据来源
+    proid: String, //产品id
+    type: {
+        type: String,
+        default: '其它'
+    }, //数据来源
     name: String, //客户名称
     phone: String, //电话
-    submitby:String, //提交人
-    handler:String, //处理人
-    path:String, //跳转路径
-    read:String, //是否已处理
+    submitby: String, //提交人
+    handler: {
+        type: String,
+        default: 'all'
+    }, //处理人
+    path: String, //跳转路径
+    read: String, //是否已处理
     time: String
 })
 const agents = mongoose.model('agents', agentSchema)
 
 module.exports = {
-  agents
+    agents
 }
