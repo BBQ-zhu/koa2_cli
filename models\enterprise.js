f936f114ede07d8f7124bae7db478b447716306c
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-11-13 21:15:08 +0800

    Update models\enterprise.js

--- models\enterprise.js
+++ models\enterprise.js
@@ -3,26 +3,38 @@ const mongoose = require('mongoose')
 
 //企业资料 
 const enterpriseSchema = new mongoose.Schema({
-  type:{type:String,default:''}, //代理记账、企业注册
-  entername:{type:String,default:''}, //企业名称
-  name:String, //负责人姓名
-  phone:String, //负责人电话
-  gener:String, //法人
-  zone:{type:String,default:''}, //区域
-  fund: String, //注册资金
-  super:String, //公司监事
-  main:String, //主营项目
-  scope:String, //经营范围
-  accout:String, //天府通账号
-  manager1: String, //客户经理
-  manager2: String, //权证经理
-  manager3: String, //审核经理
-  status: String, //审核状态
-  remarks:String, //备注
-  time:{type:String,default:''}
+    type: {
+        type: String,
+        default: ''
+    }, //代理记账、企业注册
+    entername: {
+        type: String,
+        default: ''
+    }, //企业名称
+    name: String, //负责人姓名
+    phone: String, //负责人电话
+    gener: String, //法人
+    zone: {
+        type: String,
+        default: ''
+    }, //区域
+    fund: String, //注册资金
+    super: String, //公司监事
+    main: String, //主营项目
+    scope: String, //经营范围
+    accout: String, //天府通账号
+    manager1: String, //客户经理
+    manager2: String, //权证经理
+    manager3: String, //审核经理
+    status: String, //审核状态
+    remarks: String, //备注
+    time: {
+        type: String,
+        default: ''
+    }
 })
 const enterprise = mongoose.model('enterprise', enterpriseSchema)
 
 module.exports = {
-  enterprise
+    enterprise
 }
