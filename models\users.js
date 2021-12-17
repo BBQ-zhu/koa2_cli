6c39c2aee44e3818dc13e2bd10acccf2e5cd9f97
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-12-17 17:46:40 +0800

    Update models\users.js

--- models\users.js
+++ models\users.js
@@ -1,22 +1,44 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    uid:String, //员工ID
-    username:String, //名称
-    password:{type:String,default: '123456'},
-    role:{type:String,default: ''}, //角色
-    team:{type:String,default: ''}, //团队
-    phone:{type:String,default: ''}, //电话
-    imgurl:{type:String,default: ''}, //角色
-    idcard:String, //身份证
-    address:String, //家庭住址
-    isrecomed:String, //是否推荐至咨询顾问
-    intro:String, //顾问介绍
-    remarks:String, //备注
-    time:{type:String,default: ''},
+    uid: String, //员工ID
+    username: String, //名称
+    password: {
+        type: String,
+        default: '123456'
+    },
+    role: {
+        type: String,
+        default: ''
+    }, //角色
+    team: {
+        type: String,
+        default: ''
+    }, //团队
+    phone: {
+        type: String,
+        default: ''
+    }, //电话
+    imgurl: {
+        type: String,
+        default: ''
+    }, //角色
+    idcard: String, //身份证
+    address: String, //家庭住址
+    isrecomed: {
+        type: String,
+        default: 'false'
+    }, //是否推荐至咨询顾问
+    intro: String, //顾问介绍
+    seedata: String, //大数据查询
+    remarks: String, //备注
+    time: {
+        type: String,
+        default: ''
+    },
 })
 
-const User = mongoose.model('users',userSchema)
+const User = mongoose.model('users', userSchema)
 module.exports = {
-  User
+    User
 }
