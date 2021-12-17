829fec9b2f049f6f2d779ef38c6fd7b9d7d99880
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-12-17 17:45:42 +0800

    Update models\recruiting.js

--- models\recruiting.js
+++ models\recruiting.js
@@ -2,15 +2,19 @@ const mongoose = require('mongoose')
 
 //招聘专区表
 const RecruitingSchema = new mongoose.Schema({
-    name:String, //岗位
-    salary:String, //薪资
-    address:String, //地址
-    education:String, //学历
-    details:String, //详情
-    time:{type:String,default:''}
+    companyname:String, //公司名称
+    name: String, //岗位
+    salary: String, //薪资
+    address: String, //地址
+    education: String, //学历
+    details: String, //详情
+    time: {
+        type: String,
+        default: ''
+    }
 })
-const Recruiting = mongoose.model('recruitings',RecruitingSchema)
+const Recruiting = mongoose.model('recruitings', RecruitingSchema)
 
 module.exports = {
-  Recruiting
+    Recruiting
 }
