cc4e939791b4ef9ebda622b37bbf508d8a7d1ae3
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-12-17 17:42:24 +0800

    Update models\enterprise.js

--- models\enterprise.js
+++ models\enterprise.js
@@ -13,6 +13,7 @@ const enterpriseSchema = new mongoose.Schema({
     }, //企业名称
     name: String, //负责人姓名
     phone: String, //负责人电话
+    idcard: String, //负责人身份证号码
     gener: String, //法人
     zone: {
         type: String,
@@ -23,11 +24,14 @@ const enterpriseSchema = new mongoose.Schema({
     main: String, //主营项目
     scope: String, //经营范围
     accout: String, //天府通账号
+    hometeam: String, //归属团队
     manager1: String, //客户经理
-    manager2: String, //权证经理
-    manager3: String, //审核经理
+    manager2: String, //金融客服
+    manager3: String, //代办客服
     status: String, //审核状态
+    feedback: String, //反馈消息
     remarks: String, //备注
+    schedate: Number, //审核时间
     time: {
         type: String,
         default: ''
